f9fec0a159c3f9f8c58d02c79d9d2457fbc0eaef
Fazzil <fazzil349@gmail.com>
2025-09-18 20:14:11 +0300

    Number 1

--- src/Primes.java
+++ src/Primes.java
@@ -9,8 +9,8 @@ public class Primes {
     }
 
     public static boolean isPrime(int n) {
-        if (n <= 1) return false;
         if (n == 2) return true;
+        if (n <= 1) return false;
         if (n % 2 == 0) return false;
         int limit = (int) Math.sqrt(n);
         for (int i = 3; i <= limit; i += 2) {
